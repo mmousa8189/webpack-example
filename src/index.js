62c0c9d4510f3cbe905d4b0f0466ed859e2bb255
Mohamed Mousa <mohamedamousa00@gmail.com>
2021-10-20 02:34:53 +0200

    add asset/resource rukes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import panzoom from "panzoom";
 import flicking from "@egjs/flicking";
 import commponent from "./component";
 import "./style.css";
+import "./image-component";
 
 document.body.appendChild(commponent());
 console.log(panzoom);
